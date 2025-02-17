bd14d6b628b76f8faa289aaf50921712d3385a17
Lucas <lucascs1337@gmail.com>
2025-02-16 23:28:13 -0300

    Alterado todos os arquivos para seguir o padrão da video-aulas, mostrar o free e o botão iniciar usando v-bind, adiciona o valor no curso,

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-    var app = new Vue({
+
+var app = new Vue({
     el: "#app",
     data: {
-        course: "D-Já Vue - Uma jornada pela Web",
-        image: "./images/dejavu_matrix.jpg",
-        description:
-        "Como construir aplicações web com agilidade e qualidade.",
-        duration: 42,
-        ativo: false,
+      course: "D-Já Vue - Uma jornada pela Web",
+      image: "./images/dejavu_matrix.jpg",
+      description: "Como construir aplicações web com agilidade e qualidade.",
+      duration: 42,
+      ativo: true,
+      valor: 100,
     },
-    });
-
-
+  });
+  

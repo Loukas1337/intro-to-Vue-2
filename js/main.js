c7d5978046f7c82d8c8c0988e34db29df812c3a9
Lucas <lucascs1337@gmail.com>
2025-02-16 23:53:08 -0300

    Utiliza loop na lista de cursos para não precisar repetir cada item (li), editado arquivo main.js (inclusão do loop) e adicionado imagens dos outros cursos

--- js/main.js
+++ js/main.js
@@ -1,13 +1,36 @@
-
 var app = new Vue({
     el: "#app",
     data: {
-      course: "D-Já Vue - Uma jornada pela Web",
-      image: "./images/dejavu_matrix.jpg",
-      description: "Como construir aplicações web com agilidade e qualidade.",
-      duration: 42,
-      ativo: true,
-      valor: 100,
+      courses: [
+        {
+          id: 1,
+          course: "D-Já Vue - Uma jornada pela Web",
+          image: "./images/dejavu_matrix.jpg",
+          description: "Como construir aplicações web com agilidade e qualidade.",
+          duration: 42,
+          active: true,
+          price: 100,
+        },
+        {
+          id: 2,
+          course: "Git - Via terminal para aprender da forma correta!",
+          image: "./images/logo-git-430-215-v3.jpg",
+          description:
+            "Entendendo Git através dos primeiros comandos via linha de comando.",
+          duration: 21,
+          active: false,
+          price: 900,
+        },
+        {
+          id: 3,
+          course: "Linux - Entendendo a tela preta!",
+          image: "./images/logo-linux960x430.jpg",
+          description:
+            "Entendendo Git através dos primeiros comandos via linha de comando.",
+          duration: 21,
+          active: true,
+          price: 0,
+        },
+      ],
     },
   });
-  
